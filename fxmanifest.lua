1792f6c80c9068ea4a8333425eeac5ec48425d3e
ラクーン <58646028+Raccoon-san@users.noreply.github.com>
2020-11-16 05:48:55 -0800

    Add project files.

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Kyler Gonzales'
+description 'An attempt to work with the Invision Community REST API to setup whitelisting'
+version '1.0.0'
+
+server_only 'yes'
+
+server_script 'invision_whitelist.net.dll'
---
+++ whitelist.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using RestSharp;
+using System.Net;
+using CitizenFX.Core;
+using Newtonsoft.Json.Linq;
+
+namespace invision_whitelist
+{
+    public class whitelist : BaseScript
+    {
+        string communityURL = "http://statewiderp.net/api/";
+        string apiKey = "33aef123ad89f7865f5c41d7df43ac2e";
+        string steamHex;
+        bool isWhitelisted = false;
+        JObject jsonRet;
+        public whitelist()
+        {
+            EventHandlers["playerConnecting"] += new Action<Player, string, dynamic, dynamic>(handleConnection);
+        }
+        
+        private async void handleConnection([FromSource]Player player, string plrName, dynamic kickReason, dynamic deferrals)
+        {
+
+            deferrals.defer();
+            await Delay(0);
+            deferrals.update("We're checking your whitelist. Please standby");
+            Debug.WriteLine($"Connecting user: {player.Name} (Steam: {player.Identifiers["steam"]}, Discord: {player.Identifiers["discord"]}, IP: {player.Identifiers["ip"]})");
+            
+            if(player.Identifiers["steam"] == null)
+            {
+                deferrals.done("Couldn't find your steam hex! Try re-opening FiveM with Steam open.");
+                return;
+            } else
+            {
+                steamHex = player.Identifiers["steam"].ToLower().Replace("steam:", "");
+            }
+
+            IRestClient client = new RestClient(communityURL);
+            IRestRequest request = new RestRequest("core/members?group=25&perPage=500&key=" + apiKey);
+            IRestResponse response = client.Execute(request);
+            if (response.StatusCode == (HttpStatusCode)200)
+            {
+                try
+                {
+                    jsonRet = JObject.Parse(response.Content);
+                    foreach(JToken obj in jsonRet["results"])
+                    {
+                        string retrievedHex = obj["customFields"]["1"]["fields"]["3"]["value"].ToString().ToLower().Replace("steam:", "");
+                        if(retrievedHex == steamHex)
+                        {
+                            isWhitelisted = true;
+                            break;  
+                        }
+                    }
+                } catch(Exception e)
+                {
+                    Debug.WriteLine(e.ToString());
+                    deferrals.done("An error occured with the whitelist.");
+                    return;
+                }
+            }
+            else
+            {
+                Debug.WriteLine(response.ErrorMessage);
+                deferrals.done("Could not query API. Try again later.");
+                return;
+            }
+
+            if(isWhitelisted)
+            {
+                Debug.WriteLine($"{player.Name} is whitelisted. Allowing connection.");
+                deferrals.update("You are whitelisted. Redirecting you!");
+                await Delay(500);
+                deferrals.done();
+            } else
+            {
+                Debug.WriteLine($"{player.Name} is not whitelisted. Terminating connection.");
+                deferrals.done("You are not whitelisted!");
+                return;
+            }
+        }
+    }
+}
